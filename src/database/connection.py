dacac94f0d1860a52739c48c3aea96a275974f21
Marcos Martins <contato@marcosmartins.me>
2022-05-09 14:57:16 -0300

    :lock: Improves security of database connections.
    Improves security of database connections by using Dynaconf to load secrets. Also provide a connection by context manager preventing open connections on the database.

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,17 +1,40 @@
+from contextlib import contextmanager
+
 import psycopg2
 import psycopg2.extensions
-from contextlib import contextmanager
+from dynaconf import settings
 
 
 @contextmanager
-def connect_postgres() -> psycopg2.extensions.connection:
+def connect_postgres(env: str = 'development') -> psycopg2.extensions.connection:
+    """Safety connect to Postgres.
+
+    Keyword Arguments
+    -----------------
+        env `str`: Environment to load settings. (default = 'development')
+
+    Returns
+    -------
+        `psycopg2.extensions.connection`: Returns a Postgres connection object.
+
+    Example usage
+    -------------
+    >>> with connect_postgres(env = 'production') as connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT version()')
+            print(cursor.fetchone())
+    ('PostgreSQL 14.2 (Debian 14.2-1.pgdg110+1)
+    """
+
+    env_settings = settings.from_env(env)
+
     connection = (
         psycopg2.connect(
-            host = 'localhost',
-            database = 'sicooperative',
-            port = '5432',
-            user = 'root',
-            password = 'root'
+            host = env_settings.HOST,
+            database = env_settings.DATABASE,
+            port = env_settings.PORT,
+            user = env_settings.USERNAME,
+            password = env_settings.PASSWORD
         )
     )
 
